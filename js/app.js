b3158f1faf2c895006582efb831696b4502e11e8
Rememberautumn <4013465w@sina.com>
2016-03-29 22:05:49 +0800

    merge dev

--- js/app.js
+++ js/app.js
@@ -1,30 +1,35 @@
-Vue.config.debug = true
+Vue.config.debug = false
 	/*
 	 * 配置参数
 	 */
 app = {
 		basic_url: 'http://www.fddcn.cn/wp-json/wp/v2/',
-		per_page:20
+		per_page: 20,
+		category:[{name:'CSS',catid:65},
+				{name:'HTML',catid:59},
+				{name:'JS',catid:24},
+				{name:"PHP",catid:98}
+		]
 	}
 	/*
 	 * 组件
 	 */
 	//头部组件
 var MyHeader = Vue.extend({
-		props: ['left-icon', 'title', 'right-icon'],
+		props: ['lefticon', 'title', 'righticon'],
 		template: '<header class="mui-bar mui-bar-nav">' +
-			'<a class="mui-action-back mui-icon {{left-icon}} mui-pull-left"></a>' +
+			'<a id="lefticon" class=" mui-icon {{lefticon}} mui-pull-left"></a>' +
 			'<h1 class="mui-title">{{title}}</h1>' +
-			'<a class="mui-action-back mui-icon {{right-icon}} mui-pull-right"></a>' +
+			'<a class="mui-action-back mui-icon {{righticon}} mui-pull-right"></a>' +
 			'</header>'
 	})
 	//图文列表组件
 var MyImgList = Vue.extend({
-		props: ['title', 'excerpt'],
-		template: '<li class="mui-table-view-cell mui-media">' +
-			'<a href="javascript:;">' +
-			//			'<img class="mui-media-object mui-pull-right" src="./img/shuijiao.jpg">' +
-			'<div class="mui-media-body">' +
+		props: ['title', 'excerpt', 'id'],
+		template: '<li class="mui-table-view-cell mui-media" data-id="{{id}}">' +
+			'<a href="javascript:;" data-id="{{id}}">' +
+			//'<img class="mui-media-object mui-pull-right" src="./img/shuijiao.jpg">' +
+			'<div class="mui-media-body" >' +
 			'{{title}}' +
 			'<p class="mui-ellipsis">{{{excerpt}}}</p>' +
 			'</div>' +
@@ -43,16 +48,13 @@ var WP = function() {
 }
 WP.prototype.getData = function(inData) {
 	var url = inData.url || app.basic_url;
-	inData.data = inData.data||{};
+	inData.data = inData.data || {};
 	inData.data.per_page = app.per_page;
 	mui.ajax(url + inData.route, {
 		data: inData.data,
 		dataType: 'json',
 		type: 'get',
 		success: inData.success,
-		error: function() {
-			//			alert('出错啦')
-			console.log('ajax出错啦')
-		},
+		error: inData.error,
 	})
 }
